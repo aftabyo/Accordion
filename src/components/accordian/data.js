7285d473fbd9a28df242f5bfe0033cc70a348750
Aftab <aftablue@gmail.com>
2024-02-24 00:03:35 +0530

    final commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,20 @@
+const data=[
+    {
+        id:'1',
+        question:'hi how r u',
+        answer: 'im fine'
+    },
+    {
+        id:'2',
+        question: 'aur bata',
+        answer:'badiya hai , tu bata'
+    },
+    {
+        id: '3',
+        question: 'aur',
+        answer: 'kuch ni'
+    },
+
+];
+
+export default data;
